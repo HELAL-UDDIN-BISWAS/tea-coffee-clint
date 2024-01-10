9998b1a2bbdeac597f429cb4e8d24c73b0c6de26
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-01-10 20:44:22 +0600

    gate data

--- src/Pages/Page/AllProdects/Prodects.jsx
+++ src/Pages/Page/AllProdects/Prodects.jsx
@@ -1,5 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
 
 const Prodects = () => {
+    const { data } = useQuery({
+        queryKey: ['data'],
+        queryFn: () =>
+          fetch('/data.json').then((res) =>
+            res.json(),
+
+          ),
+      })
+      console.log(data)
     
     return (
         <div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,19 @@ import {
 } from "react-router-dom";
 import router from './Route/Route.jsx';
 import Proveider from './Proveider/Proveider.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Proveider>
       <div className='max-w-7xl mx-auto'>
-      <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </div>
     </Proveider>
   </React.StrictMode>,
