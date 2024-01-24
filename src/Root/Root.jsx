7e1e9e18f471edb9f4dac3a98cfd6761558c714c
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-01-24 19:55:18 +0600

    Update Root file

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navber from '../Shared/Navber/Navber';
+import Footer from '../Shared/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
