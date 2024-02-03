546346d34bf0d4af4e6507b704b1d15f0395c046
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-02-03 20:04:57 +0600

    add some code

--- src/Pages/Page/DetailsProduct/Detailsproduct.jsx
+++ src/Pages/Page/DetailsProduct/Detailsproduct.jsx
@@ -1,11 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 
 const Detailsproduct = () => {
-    const productData=useLoaderData();
+    const productData = useLoaderData();
     console.log(productData)
     return (
-        <div>
-            <img src={productData.image_url} alt="" />
+        <div className="max-w-6xl mx-auto">
+            <div className="md:flex">
+                <div className="flex-1">
+                    <img className="rounded" src={productData.image_url} alt="" />
+                </div>
+                <div className="flex-1 ml-10">
+                    <p className="text-2xl text-slate-600 ">Type: <span className="text-black">{productData.type}</span></p>
+                    <p className="text-2xl text-slate-600">Flavor: <span className="text-black">{productData.flavor}</span></p>
+                    <p className="text-2xl text-slate-600">Caffeine Content: <span className="text-black">{productData.caffeine_content}</span></p>
+                    <h3 className="text-2xl text-slate-600">Price: <span className="text-black">{productData.price}</span></h3>
+
+                    <button className=" hover:bg-slate-400 rounded">BY NOW</button>
+                </div>
+            </div>
             deta
         </div>
     );
