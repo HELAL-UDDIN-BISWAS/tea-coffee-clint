a9dba1d5afe61a27b36b1687839c3709f8a1d575
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-02-08 21:43:44 +0600

    create form with contect us page

--- src/Pages/Page/Contect/Contect.jsx
+++ src/Pages/Page/Contect/Contect.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Contect = () => {
     return (
-        <div>
-            contect
+        <div className="min-h-[60vh] flex mt-14 mb-20">
+
+        <div className="md:flex gap-20 md:w-9/12 items-center justify-center p-3 mx-auto">
+            <form ref={form} onSubmit={sendEmail} className="flex flex-1  flex-col space-y-5">
+                <input required type="text" name="user_name" placeholder="Name" className="bordered input border rounded-full bg-slate-100 p-4  " />
+                <input required type="email" name="user_email" placeholder="Email" className="bordered input border rounded-full bg-slate-100 p-4  " />
+                <textarea required name="message" placeholder="Write your Message" className=" rounded-full bg-slate-100 p-4 h-20  "></textarea>
+                <input type="submit" className="btn border rounded-full bg-slate-100 p-4  " />
+            </form>
+            <div className="flex-1 mt-10">
+            <div>
+                <h3 className="flex items-center text-gray-500 text-2xl gap-2"><MdEmail /> <p>mdhelal6775@gmail.com</p></h3>
+                <h3 className="flex items-center text-gray-500 text-2xl gap-2"><FaPhone/><p>01858179115</p></h3>
+             
+                <div className="flex gap-2  text-white">
+                    <Link className="m-4 text-xl bg-gray-500 rounded p-2"><FaFacebookF></FaFacebookF></Link>
+                    <Link className="m-4 text-xl bg-gray-500 rounded p-2"><FaGoogle></FaGoogle></Link>
+                    <Link className="m-4 text-xl bg-gray-500 rounded p-2"><FaGithub></FaGithub></Link>
+                </div>
+            </div>
+            </div>
         </div>
+    </div>
     );
 };
 
