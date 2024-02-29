c8703c88330d4a5898c33c3f78b17ecc5cae9f36
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-02-29 19:52:10 +0600

    ubdate add prodect page

--- src/Dashboard/pages/AddProdect/AddProdect.jsx
+++ src/Dashboard/pages/AddProdect/AddProdect.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProdect = () => {
+    const { register, handleSubmit } = useForm();
+   const onSubmit=(data)=>{
+   console.log(data.type)
+   }
     return (
         <div className=' md:ml-10 m-4'>
-           <form className=''>
+           <form className=''  onSubmit={handleSubmit(onSubmit)}>
            <div className=' grid  md:grid-cols-2 gap-3'>
                     <div className="w-full">
                         <label className='block'>
                             <span>Type</span>
                         </label>
-                        <input type="text" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product Type' required />
+                        <input {...register('type')} type="text" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product Type' required />
                     </div>
 
                     <div className="w-full">
@@ -51,19 +56,20 @@ const AddProdect = () => {
                         <label className='block'>
                             <span>Product Name</span>
                         </label>
-                        <input type="password" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product_Name' required />
+                        <input type="text" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product_Name' required />
                     </div>
 
                     <div className="w-full">
                         <label className='block'>
                             <span>Image</span>
                         </label>
-                        <input type="password" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product_Name' required />
+                        <input type="text" className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product_Name' required />
                     </div>
 
                    
                 </div>
-                <textarea required name="message" placeholder="Write your Message" className=" rounded bg-slate-100 p-3 h-20 md:w-96 my-3"></textarea>
+                <textarea required name="message" placeholder="Long Description " className=" rounded bg-slate-100 p-3 h-20 md:w-96 my-3"></textarea>
+                <button>dfsdfd</button>
            </form>
         </div>
     );
