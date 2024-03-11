4f7a7191b1e1a1b938fd575b8c67e3f6390cf92d
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-03-11 13:10:56 +0600

    show modal

--- src/Pages/Page/DetailsProduct/Detailsproduct.jsx
+++ src/Pages/Page/DetailsProduct/Detailsproduct.jsx
@@ -1,10 +1,21 @@
 import { useLoaderData } from "react-router-dom";
-import { Button } from "keep-react";
 import { GrShop } from "react-icons/gr";
+import { useState } from "react";
+import { Button, Modal } from "keep-react";
+import { CloudArrowUp } from "phosphor-react";
+
+
 const Detailsproduct = () => {
     const productData = useLoaderData();
-    console.log(productData)
-    return (
+        const [showModalX, setShowModalX] = useState(false);
+    const onClickOne = () => {
+        console.log('Hello World')
+        setShowModalX(!showModalX);
+      };
+    const onClickTwo = () => {
+        setShowModalX(!showModalX);
+    };
+        return (
         <div className="max-w-6xl mx-auto">
             <div className="md:flex">
                 <div className="flex-1">
@@ -17,15 +28,38 @@ const Detailsproduct = () => {
                     <h3 className="text-2xl text-slate-600">Price: <span className="text-black">{productData.price}</span></h3>
 
                     {/* <button className=" hover:bg-slate-400 bg-slate-300 py-2 px-4 rounded">BY NOW</button> */}
-                    <Button className="items-center justify-center my-8" size="md" color="success"><GrShop className="mx-1 items-center" />BY NOW</Button>
+                    <Button className="items-center justify-center my-8" onClick={onClickTwo} size="md" color="success"><GrShop className="mx-1 items-center" />BY NOW</Button>
+                  <Modal
+                        icon={<CloudArrowUp size={28} color="#1B4DFF" />}
+                        size="md"
+                        show={showModalX}
+                        onClose={onClickTwo}
+                    >
+                        <Modal.Header>Do you want to upload this file?</Modal.Header>
+                        <Modal.Body>
+                            <div className="space-y-6">
+                                <p className="text-body-5 md:text-body-4 leading-relaxed text-metal-500">
+                                    Contrary to popular belief, Lorem Ipsum is not simply random text.
+                                </p>
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button type="outlineGray" onClick={onClickTwo}>
+                                Cancel
+                            </Button>
+                            <Button type="primary" onClick={onClickOne}>
+                                Confirm
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
             <div className="">
                 <h2 className="text-2xl py-5">{productData?.short_description}</h2>
                 <p className="max-w-4xl ">{productData?.long_description}</p>
-            deta
+                deta
             </div>
-            
+
         </div>
     );
 };
