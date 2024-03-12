a22c5e386f16cb69c6d3a4322efd2b15dfcf5e14
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-03-12 10:18:57 +0600

    add chart

--- src/Dashboard/pages/DashboardChart/DashboardChart.jsx
+++ src/Dashboard/pages/DashboardChart/DashboardChart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Chart from './Chart/Chart';
 
 const DashboardChart = () => {
     return (
         <div>
+            <Chart></Chart>
             DashboardChart
         </div>
     );
