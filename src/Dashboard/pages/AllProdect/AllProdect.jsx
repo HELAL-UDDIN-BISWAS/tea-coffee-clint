e952dcd9202a16a36654d80ab63389f707d5f6e4
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-03-23 12:07:07 +0600

    modal form in edit product

--- src/Dashboard/pages/AllProdect/AllProdect.jsx
+++ src/Dashboard/pages/AllProdect/AllProdect.jsx
@@ -2,8 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import Swal from "sweetalert2";
 import { SkeletonComponent } from "../../../Components/Skeleton/SkeletonComponent";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Proveider/Proveider";
+import { useContext, useState } from "react";
+
+import { Button, Modal } from "keep-react";
+import { CloudArrowUp } from "phosphor-react";
+import { useForm } from "react-hook-form";
+
 
 const AllProdect = () => {
+  const {user}=useContext(AuthContext);
+  const { register, handleSubmit } = useForm();
+  const [showModalX, setShowModalX] = useState(false);
    const {refetch, isLoading, data:AllData } = useQuery({
         queryKey: [],
         queryFn: () => fetch('https://tea-coffee-server.vercel.app/allproducts')
@@ -45,6 +55,12 @@ const AllProdect = () => {
         <SkeletonComponent></SkeletonComponent>
       </div>
     }
+    const onSubmit=(data)=>{
+
+    }
+    const onClickTwo = () => {
+      setShowModalX(!showModalX);
+  };
     return (
         <div className="p-2">
             <div className="max-w-screen-md mx-auto">
@@ -69,8 +85,89 @@ const AllProdect = () => {
                             <td className="py-2 px-4 border-b">{datas.price}</td>
                             <td className="py-2 px-4 border-b">{datas.flavor}</td>
                             <td className="py-2 px-4 border-b">
-                                <button className="text-blue-500">Edit</button>
+                                <button onClick={onClickTwo} className="text-blue-500">Edit</button>
                                 <button onClick={()=>deleteData(datas._id)} className="text-red-500 ml-2">Delete</button>
+                                <Modal
+                        size='lg'
+                        show={showModalX}
+                        onClose={onClickTwo}
+                    >
+                        <Modal.Header>Do you want to upload this file?</Modal.Header>
+                        <Modal.Body>
+                            <div className="space-y-6">
+                                <form className='' onSubmit={handleSubmit(onSubmit)}>
+                                <div className=' grid md:grid-cols-2'>
+                    <div className="">
+                        <label className='block'>
+                            <span>Type</span>
+                        </label>
+                        <input {...register('type')} type="text" className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product Type' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Price</span>
+                        </label>
+                        <input type="text" {...register('price')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Price' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Origin</span>
+                        </label>
+                        <input type="text" {...register('origin')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Origin' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Flavor</span>
+                        </label>
+                        <input type="text" {...register('flavor')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Flavor' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Caffeine Content</span>
+                        </label>
+                        <input type="text" {...register('caffeinecontent')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='caffeine_content' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Short Description</span>
+                        </label>
+                        <input type="text" {...register('shortdescription')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='short_description' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Product Name</span>
+                        </label>
+                        <input type="text" {...register('productname')} className="bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Product_Name' required />
+                    </div>
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Image</span>
+                        </label>
+                        <input {...register('photo')} type="file" required />
+                    </div>                
+
+                    <div className="w-full">
+                        <label className='block'>
+                            <span>Long Description</span>
+                        </label>
+                        <input type="text" {...register('longdescription')}  className="md:w-72 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Long Description' required />
+                    </div>     
+                </div>
+                
+                <button className='bg-lime-100 text-lime-500 hover:text-white hover:bg-lime-600 py-2 px-3 rounded my-8'>Add Product</button>
+                                </form>
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        </Modal.Footer>
+                    </Modal>
                             </td>
                         </tr>)
                         }                   
