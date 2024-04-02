cb2e1c63a8090649f118ed5554833947ea4d0cbb
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-04-02 22:23:49 +0600

    comment section input fild create

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -2,22 +2,25 @@
 const Comment = () => {
     const handleSubmit=(event)=>{
         event.preventDefault();
+        const form = event.target
+        const Message=form.comment.value;
+        console.log(Message)
       console.log('hello world')
     }
 
     return (
         <div>
             <div>
-                <form>
+                <form onSubmit={handleSubmit} >
                <div className="w-full">
                         <label className='block mb-2'>
                             <span className='text-2xl  m-4'>Massage</span>
                         </label>
-                        <input type="text" name='text' className="lg:w-[500px] md:w-96 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Inter Your Message' required />
+                        <input type="text" name='comment' className="lg:w-[500px] md:w-96 bg-white px-4 py-2 text-lg outline-none border-2 rounded hover:border-gray-600 border-gray-400 duration-200 bg-inherit" placeholder='Inter Your Message' required />
                     </div>
+                      
                     <div className="items-center text-center justify-center mb-3">
-                    <button onClick={handleSubmit} className=" hover:bg-[#225af2] bg-[#053cd9] py-2 rounded text-white px-4">Comment</button>
-                        
+                    <button type="submit" className=" hover:bg-[#225af2] bg-[#053cd9] py-2 rounded text-white px-4">Comment</button>   
                     </div>
                 </form>
             </div>
